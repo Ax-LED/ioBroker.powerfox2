49c2b0632b9196484c1629c330394227165f1fb1
Ax-LED <37046881+Ax-LED@users.noreply.github.com>
2026-03-15 14:47:45 +0000

    Fix remaining ESLint issues

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,19 +1,15 @@
-import config from '@iobroker/eslint-config';
+import config from "@iobroker/eslint-config";
 
 export default [
-    ...config,
-    {
-        ignores: [
-            'admin/**',
-            'test/**',
-            '*.test.js',
-        ],
+  ...config,
+  {
+    ignores: ["admin/**", "test/**", "*.test.js", "lib/**", "**/*.d.ts"],
+  },
+  {
+    rules: {
+      "no-console": "off",
+      "no-trailing-spaces": "error",
+      semi: ["error", "always"],
     },
-    {
-        rules: {
-            'no-console': 'off',
-            'no-trailing-spaces': 'error',
-            'semi': ['error', 'always'],
-        },
-    },
+  },
 ];
